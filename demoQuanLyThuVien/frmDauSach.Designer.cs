6a50b29c3b67f8f22294722b48390f73b82bd764
ThuyThuong <admin@desktop>
2021-12-22 22:51:49 +0700

    xây dựng form Đầu Sách

---
+++ demoQuanLyThuVien/frmDauSach.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace demoQuanLyThuVien
+{
+    partial class frmDauSach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHoantat = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnTroVe = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.lblDauSach = new System.Windows.Forms.Label();
+            this.lblQLDS = new System.Windows.Forms.Label();
+            this.txtTenDS = new System.Windows.Forms.TextBox();
+            this.lblDS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHoantat
+            // 
+            this.btnHoantat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoantat.Location = new System.Drawing.Point(290, 200);
+            this.btnHoantat.Name = "btnHoantat";
+            this.btnHoantat.Size = new System.Drawing.Size(77, 29);
+            this.btnHoantat.TabIndex = 46;
+            this.btnHoantat.Text = "Hoàn tất";
+            this.btnHoantat.UseVisualStyleBackColor = true;
+            this.btnHoantat.Click += new System.EventHandler(this.btnHoantat_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(224, 249);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(77, 29);
+            this.btnSua.TabIndex = 45;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(356, 249);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(77, 29);
+            this.btnXoa.TabIndex = 44;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(93, 249);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(77, 29);
+            this.btnThem.TabIndex = 43;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(77, 305);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(363, 194);
+            this.listView1.TabIndex = 42;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(390, 94);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(92, 30);
+            this.btnTimKiem.TabIndex = 41;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnTroVe
+            // 
+            this.btnTroVe.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTroVe.Location = new System.Drawing.Point(161, 200);
+            this.btnTroVe.Name = "btnTroVe";
+            this.btnTroVe.Size = new System.Drawing.Size(77, 29);
+            this.btnTroVe.TabIndex = 40;
+            this.btnTroVe.Text = "Trở về";
+            this.btnTroVe.UseVisualStyleBackColor = true;
+            this.btnTroVe.Click += new System.EventHandler(this.btnTroVe_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(138, 98);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(246, 25);
+            this.txtTimKiem.TabIndex = 39;
+            // 
+            // lblDauSach
+            // 
+            this.lblDauSach.AutoSize = true;
+            this.lblDauSach.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDauSach.Location = new System.Drawing.Point(44, 101);
+            this.lblDauSach.Name = "lblDauSach";
+            this.lblDauSach.Size = new System.Drawing.Size(88, 17);
+            this.lblDauSach.TabIndex = 38;
+            this.lblDauSach.Text = "Mã đầu sách:";
+            // 
+            // lblQLDS
+            // 
+            this.lblQLDS.AutoSize = true;
+            this.lblQLDS.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQLDS.Location = new System.Drawing.Point(102, 34);
+            this.lblQLDS.Name = "lblQLDS";
+            this.lblQLDS.Size = new System.Drawing.Size(302, 32);
+            this.lblQLDS.TabIndex = 37;
+            this.lblQLDS.Text = "QUẢN LÝ ĐẦU SÁCH";
+            // 
+            // txtTenDS
+            // 
+            this.txtTenDS.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenDS.Location = new System.Drawing.Point(138, 158);
+            this.txtTenDS.Name = "txtTenDS";
+            this.txtTenDS.Size = new System.Drawing.Size(246, 25);
+            this.txtTenDS.TabIndex = 48;
+            // 
+            // lblDS
+            // 
+            this.lblDS.AutoSize = true;
+            this.lblDS.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDS.Location = new System.Drawing.Point(33, 161);
+            this.lblDS.Name = "lblDS";
+            this.lblDS.Size = new System.Drawing.Size(99, 17);
+            this.lblDS.TabIndex = 47;
+            this.lblDS.Text = "Tên đầu sách  :";
+            // 
+            // frmDauSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 511);
+            this.Controls.Add(this.txtTenDS);
+            this.Controls.Add(this.lblDS);
+            this.Controls.Add(this.btnHoantat);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnTroVe);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.lblDauSach);
+            this.Controls.Add(this.lblQLDS);
+            this.Name = "frmDauSach";
+            this.Text = "frmDauSach";
+            this.Load += new System.EventHandler(this.frmDauSach_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHoantat;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnTroVe;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label lblDauSach;
+        private System.Windows.Forms.Label lblQLDS;
+        private System.Windows.Forms.TextBox txtTenDS;
+        private System.Windows.Forms.Label lblDS;
+    }
+}
---
+++ demoQuanLyThuVien/frmDauSach.cs
@@ -0,0 +1,132 @@
+﻿using demoQuanLyThuVien.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoQuanLyThuVien
+{
+    public partial class frmDauSach : Form
+    {
+        Models.ModelQLTV db = new Models.ModelQLTV();
+        public frmDauSach()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDauSach_Load(object sender, EventArgs e)
+        {
+            hienthi();
+        }
+        public void hienthi()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+            listView1.Columns.Add("Mã đầu sách", 120);
+            listView1.Columns.Add("Tên đầu sách", 200);
+            foreach(DauSach ds in db.DauSaches.ToList())
+            {
+                ListViewItem li = listView1.Items.Add(ds.mads);
+                li.SubItems.Add(ds.tends);
+            }
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach(ListViewItem li in listView1.SelectedItems)
+            {
+                txtTimKiem.Text = li.SubItems[0].Text;
+                txtTenDS.Text = li.SubItems[1].Text;
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            //DauSach ds = db.DauSaches.Find(txtTimKiem.Text);
+            //if (ds != null)
+            //{
+            //    listView1.Items.Clear();
+            //    ListViewItem li = listView1.Items.Add(ds.mads);
+            //    li.SubItems.Add(ds.tends);
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Không tìn thấy thông tin đầu sách!!!!!");
+            //}
+        }
+
+        private void btnTroVe_Click(object sender, EventArgs e)
+        {
+            //this.Hide();
+            //frmQLSach ql = new frmQLSach();
+            //ql.ShowDialog();
+        }
+
+        private void btnHoantat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //DauSach ds = db.DauSaches.Find(txtTimKiem.Text);
+            //if (ds != null)
+            //{
+            //    MessageBox.Show("Không thể thêm mới Đầu sách!!!!!!");
+            //}
+            //else
+            //{
+            //    ds = new DauSach();
+            //    ds.mads = txtTimKiem.Text;
+            //    ds.tends = txtTenDS.Text;
+            //    db.DauSaches.Add(ds);
+            //    db.SaveChanges();
+            //    hienthi();
+            //}
+            
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            //DauSach ds = db.DauSaches.Find(txtTimKiem.Text);
+            //if (ds == null)
+            //{
+            //    MessageBox.Show("Đầu sách chưa tồn tại nên không thể xóa!!");
+            //}
+            //ds.tends = txtTenDS.Text;
+            //db.SaveChanges();
+            //hienthi();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            //bool kq = false;
+            //foreach(ListViewItem li in listView1.SelectedItems)
+            //{
+            //    kq = true;
+            //    DauSach ds = db.DauSaches.Find(li.SubItems[0].Text);
+            //    if (ds != null)
+            //    {
+            //        if (ds.Saches.Count == 0)
+            //            db.DauSaches.Remove(ds);
+            //        else
+            //            MessageBox.Show("Không thể xóa!!!!");
+            //    }
+            //    db.SaveChanges();
+            //}
+            //if (kq)
+            //    hienthi();
+        }
+
+        
+    }
+}
